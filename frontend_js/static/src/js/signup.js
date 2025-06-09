e0e1b8a3d727a7978391b9f8d38eba949896b1f9
Damien Bouvy <dbo@odoo.com>
2025-06-09 09:58:26 +0200

    [SOL] country/vat fields at signup
    - extend the signup form public widget:
    	- bind to notif service for error msgs
    	- instanciate widget to control country/vat fields
    	- validate form on submit (override), notif if problematic
    - new widget to control the country/vat field
    	- qweb-js template
    	- fetch country list from server via rpc
    	- watch change event on country selection, hide/show VAT field

--- frontend_js/static/src/js/signup.js
+++ frontend_js/static/src/js/signup.js
@@ -1,4 +1,85 @@
 import publicWidget from "@web/legacy/js/public/public_widget";
 import { rpc } from "@web/core/network/rpc";
 
+// This widget controls the whole signup form
+publicWidget.registry.SignUpForm.include({
+    selector: ".oe_signup_form",
+
+    init() {
+        this._super.apply(this, arguments);
+        this.countrySelector = null; // Placeholder for the country selector widget
+        this.notification = this.bindService("notification");
+    },
+
+    start() {
+        return this._super.apply(this, arguments).then(() => {
+            const countrySelectorDiv = this.el.querySelector("#oe_signup_country_selector");
+            this.countrySelectorWidget = new CountrySelector(this);
+            countrySelectorDiv.innerHTML = ""; // Clear the div before appending the widget
+            this.countrySelectorWidget.appendTo(countrySelectorDiv);
+        });
+    },
+
+    _onSubmit(event) {
+        const countrySelect = this.el.querySelector("select[name='country_id']");
+        if (!countrySelect || !countrySelect.value) {
+            event.preventDefault();
+            this.notification.add("Please select a country.", {
+                type: "danger",
+            });
+            return;
+        }
+        const countryNeedsVat = this.countrySelectorWidget.vatRequired;
+        const vatInput = this.el.querySelector("input[name='vat']");
+        if (countryNeedsVat && !vatInput.value) {
+            event.preventDefault();
+            this.notification.add("Please provide a VAT number for European countries.", {
+                type: "danger",
+            });
+            return;
+        }
+        this._super.apply(this, arguments);
+    }
+});
+
+// This widget is only the country selector
+const CountrySelector = publicWidget.Widget.extend({
+    template: "frontend_js.country_selector",
+    events: {
+        "change select[name='country_id']": "_onCountryChange",
+    },
+
+    init() {
+        this._super.apply(this, arguments);
+        this.countries = [];
+        this.selectedCountryId = null;
+        this.vatRequired = false;
+    },
+    
+    async start() {
+        return Promise.all([
+            this._super.apply(this, arguments),
+            this._loadCountries(),
+        ]);
+    },
+
+    _loadCountries() {
+        return rpc('/signup/countries').then(countries => {
+            this.countries = countries;
+            this.renderElement();
+        });
+    },
+
+    _onCountryChange(event) {
+        const countryId = parseInt(event.target.value);
+        const country = this.countries.find(c => c.id === countryId);
+        if (!country) {
+            console.warn(`Country with ID ${countryId} not found`);
+            return;
+        }
+        this.selectedCountryId = countryId;
+        this.vatRequired = country.is_european;
+        this.renderElement();
+    },
+});
 
