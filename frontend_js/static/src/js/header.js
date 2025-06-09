b8f043d1d93324fbe489336851fb6a59ce87f3dc
Damien Bouvy <dbo@odoo.com>
2025-06-09 09:56:13 +0200

    [SOL] harassment popup
    - extend the navbar header widget
    - bind to notification service in init
    - check user object to know if user is logged
    - check localstorage key to know if notif was previously dismissed
    - otherwise display a notification
    	- button config to redirect to signup page
    	- close handler will store value in localstorage

--- frontend_js/static/src/js/header.js
+++ frontend_js/static/src/js/header.js
@@ -3,3 +3,49 @@ import { markup } from "@odoo/owl";
 import { user } from "@web/core/user";
 import { browser } from "@web/core/browser/browser";
 
+publicWidget.registry.HeaderGeneral.include({
+    init() {
+        this._super.apply(this, arguments);
+        this.notification = this.bindService("notification");
+    },
+
+    start() {
+        const self = this;
+        this._super.apply(this, arguments).then(() => {
+            if (user.userId === null && !browser.localStorage.getItem("signup_notification_dismissed")) {
+                self._showSignupNotification();
+            }
+        });
+    },
+
+    _dismissNotification() {
+        browser.localStorage.setItem("signup_notification_dismissed", "true");
+    },
+
+    _showSignupNotification() {
+        this.notification.add(
+            markup(`
+                <div>
+                    <strong>Hey there!</strong>
+                    <p>Signup to enjoy the full experience of our website.</p>
+                </div>
+            `),
+            {
+                type: "info",
+                sticky: true,
+                onClose: () => {
+                    this._dismissNotification();
+                },
+                buttons: [
+                    {
+                        name: "Sign Up",
+                        onClick: () => {
+                            window.location.href = "/web/signup";
+                        },
+                        primary: true,
+                    },
+                ],
+            },
+        );
+    },
+});
